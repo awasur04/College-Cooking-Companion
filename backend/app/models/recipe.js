80a495a5218a29aea6c252d6de3db23f90e41361
awasur04 <awasur04@yahoo.com>
2022-10-13 18:52:28 -0500

    created base recipe model

---
+++ backend/app/models/recipe.js
@@ -0,0 +1,13 @@
+class Recipe
+{
+	constructor(id, title, image, ingredients, instructions, nutrition)
+	{
+		this.id = id;
+		this.title = title;
+		this.image = image;
+		this.ingredients = ingredients;
+		this.instructions = instructions;
+		this.nutrition = nutrition;
+	}
+
+}
