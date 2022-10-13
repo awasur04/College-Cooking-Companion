663298a97c842bd11ebc572394a01045b9030f41
awasur04 <awasur04@yahoo.com>
2022-10-13 18:53:09 -0500

    added config file for api settings

---
+++ backend/app/config/api.config.js
@@ -0,0 +1,18 @@
+module.exports =
+{
+	SPOONACULAR:
+	{
+		API_KEY: "",
+		HOST: "api.spoonacular.com",
+		FIND_BY_INGREDIENTS: "api.spoonacular.com/recipes/findByIngredients",
+		//GET_INSTRUCTIONS: `https://api.spoonacular.com/recipes/${id}/findByIngredients`,
+		//GET_PDF: `https://api.spoonacular.com/recipes/${id}/card`,
+		//NUTRITON: `https://api.spoonacular.com/food/menuItems/${id}/nutritionWidget.png`,
+	},
+
+	TEST:
+	{
+		IP: "localhost:3000",
+	},
+
+}
