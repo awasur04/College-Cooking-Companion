9b8a7faa3d1876efb299210b4b09464d6cde91fb
awasur04 <awasur04@yahoo.com>
2022-10-21 13:15:24 -0500

    implemented ingredient class

---
+++ backend/app/models/ingredient.js
@@ -0,0 +1,12 @@
+class Ingredient
+{
+	constructor(id, name, image, amount, currentlyOwned)
+	{
+		this.id = id;
+		this.name = name;
+		this.image = image;
+		this.amount = amount;
+		this.currentlyOwned = currentlyOwned;
+	}
+
+}
