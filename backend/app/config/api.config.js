ab4cddd9d591ec8b85b48508defaafb6e6a9c0a8
awasur04 <awasur04@yahoo.com>
2022-10-24 12:37:34 -0500

    updated config api endpoint

--- backend/app/config/api.config.js
+++ backend/app/config/api.config.js
@@ -4,7 +4,7 @@ module.exports =
 	{
 		API_KEY: "",
 		HOST: "api.spoonacular.com",
-		FIND_BY_INGREDIENTS: "api.spoonacular.com/recipes/findByIngredients",
+		FIND_BY_INGREDIENTS: "/recipes/findByIngredients",
 		//GET_INSTRUCTIONS: `https://api.spoonacular.com/recipes/${id}/findByIngredients`,
 		//GET_PDF: `https://api.spoonacular.com/recipes/${id}/card`,
 		//NUTRITON: `https://api.spoonacular.com/food/menuItems/${id}/nutritionWidget.png`,
