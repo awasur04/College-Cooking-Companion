ca1345778fadfe0bb4fa60a57a58b0a87e7fbcd7
awasur04 <awasur04@yahoo.com>
2022-10-24 20:58:05 -0500

    cleaned up unused code

--- backend/server.js
+++ backend/server.js
@@ -7,49 +7,29 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-
 const app = express();
 const db = require("./app/models");
 const recipe_controller = require("./app/controllers/recipe_controller");
 //db.sequelize.sync();
 
-// parse requests of content-type - application/json
 app.use(bodyParser.json());
-
-// parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
-let runType = process.argv.slice(2);
 
-if (runType == "dev")
-{
-  require('child_process').fork('../testing/index.js');
-  recipe_controller.setUp(runType);
-  runServer();
-}
-else if (runType == "prod")
-{
-  recipe_controller.setUp(runType);
-  runServer();
-}
-else
-{
-  console.log("\nPlease select a run type `dev`, `prod` -- Example run case `node server.js dev`");
-}
 
-function runServer()
-{
-  // set port, listen for requests
-  const PORT = process.env.PORT || 8080;
-  app.listen(PORT, () =>
-  { 
-    console.log(`Server is running on port ${PORT}.`);
 
-    let output = recipe_controller.findRecipes(ingredients = ["eggs", "salt", "milk", "butter", "yeast"], displayCheck);
-  });
-}
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () =>
+{
+  console.log(`Server is running on port ${PORT}.`);
+
+  let output = recipe_controller.findRecipes(ingredients = ["eggs", "salt", "milk", "butter", "yeast"], displayCheck);
+});
+
 
 function displayCheck(output)
 {
   console.log(output);
 }
+
